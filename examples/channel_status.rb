1b3da15a6fa5f7aca98266bcb51a87ba011a52b2
Paul Brickfeld <paulbrickfeld@gmail.com>
2013-02-13 23:34:00 +0100

    Updated and refactored the examples

--- examples/channel_status.rb
+++ examples/channel_status.rb
@@ -1,54 +1,16 @@
 require 'cinch'
 require 'cinch/extensions/authentication'
 
-class Admin
-  include Cinch::Plugin
-  include Cinch::Extensions::Authentication
-
-  enable_authentication
-
-  match /quit/, :method => :quit
-
-  def quit(m)
-    bot.quit
-  end
-end
-
-class Quote
-  include Cinch::Plugin
-  include Cinch::Extensions::Authentication
-
-  match /quote/, :method => :quote
-  match /add (.+)/, :method => :add
-  match /delete (.+)/, :method => :delete
-
-  def initialize(*args)
-    super
-    
-    @quotes = []
-  end
-
-  def quote(m)
-    m.reply @quotes.sample
-  end
-
-  def add(m, quote)
-    m.reply 'Added quote' if @quotes << quote
-  end
-
-  def delete(m, quote)
-    # People with voice or higher may help keep the 'database' of quotes clean.
-    return unless authenticated? m, :v 
-    m.reply 'Deleted quote' if @quotes.delete quote
-  end
-end
+require_relative 'plugins'
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server                  = 'irc.freenode.org'
-    c.channels                = ['#authentication-test']
-    c.authentication          = Cinch::Configuration::Authentication
+    c.channels                = ['#cinch-authentication']
+
+    c.authentication          = Cinch::Configuration::Authentication.new
     c.authentication.strategy = :channel_status
+    c.authentication.level    = :v 
     
     c.plugins.plugins << Admin
     c.plugins.options[Admin][:authentication_level] = :o
--- examples/user_list.rb
+++ examples/user_list.rb
@@ -1,55 +1,15 @@
 require 'cinch'
 require 'cinch/extensions/authentication'
-require 'cinch/plugins/user_list'
 
-class Admin
-  include Cinch::Plugin
-  include Cinch::Extensions::Authentication
-
-  enable_authentication
-
-  match /quit/, :method => :quit
-
-  def quit(m)
-    bot.quit
-  end
-end
-
-class Quote
-  include Cinch::Plugin
-  include Cinch::Extensions::Authentication
-
-  match /quote/, :method => :quote
-  match /add (.+)/, :method => :add
-  match /delete (.+)/, :method => :delete
-
-  def initialize(*args)
-    super
-    
-    @quotes = []
-  end
-
-  def quote(m)
-    m.reply @quotes.sample
-  end
-
-  def add(m, quote)
-    m.reply 'Added quote' if @quotes << quote
-  end
-
-  def delete(m, quote)
-    # People with voice or higher may help keep the 'database' of quotes clean.
-    return unless authenticated? m, [:admins, :users]
-    m.reply 'Deleted quote' if @quotes.delete quote
-  end
-end
+require_relative 'plugins'
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server                  = 'irc.freenode.org'
-    c.channels                = ['#authentication-test']
+    c.channels                = ['#cinch-authentication']
     c.authentication          = Cinch::Configuration::Authentication.new
-    c.authentication.strategy = :user_list
+    c.authentication.strategy = :list
+    c.authentication.level    = :users
     c.authentication.admins   = ['waxjar']
     c.authentication.users    = c.authentication.admins + ['cinch_user']
     
